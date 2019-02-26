419f55d0965f33dd3618b3ce4b990c38ce9eca83
PurpleBabar <lalung.alexandre@gmail.com>
2019-02-26 11:54:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -137,7 +137,14 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperline',
+    'hyper-statusline',
+    'hyper-tabs-enhanced',
+    'hyper-pane',
+    'hyper-folder-icon',
+    'hyperterm-dibdabs'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
